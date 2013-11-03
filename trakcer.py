ef37622bf510a10ecf9aa4ffacab9980f7276f0d
vivijie <j.uloveit@gmail.com>
2013-11-03 21:34:56 +0800

    1.change date value to int  2.creat new datebase frmaework

--- trakcer.py
+++ trakcer.py
@@ -10,18 +10,21 @@ currentApp = activeAppName
 print "The currentApp is: " + currentApp
 appTime = 0
 appList = {}
-
+appArray = {}
+# Initial appArray
+timeStart = int(time.strftime('%Y%m%d%H%M%S',time.localtime(time.time())))
+appArray[timeStart] = currentApp
 
 # record time data
-for i in range(20):
+for i in range(10):
 	activeAppName = NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
 	if currentApp == activeAppName:
-		t = time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))
+		t = int(time.strftime('%Y%m%d%H%M%S',time.localtime(time.time())))
 		time.sleep(1)
 		
 		# record log
 		appTime += 1 
-		print activeAppName + "_" + t + " %d" % appTime
+		print activeAppName + "%d %d" % (t, appTime)
 
 
 		# record app time to dictionary
@@ -35,18 +38,27 @@ for i in range(20):
 
 	else:
 		currentApp = activeAppName
+		timeStart = int(time.strftime('%Y%m%d%H%M%S',time.localtime(time.time())))
+
+		appArray[timeStart] = currentApp
+		print timeStart
 		appTime = 0
 		print "Switch to: %s" % currentApp
 		continue
 
 print appList
-# write appList to csv
-with open('timedate.csv', 'wb') as f:
-	w = csv.DictWriter(f, appList.keys())
-	w.writeheader()
-	w.writerow(appList)
+print appArray
+# # sort the appArray
+# appArraySorted = sorted(appArray.iteritems(), key=lambda d:d[0])
+# print appArraySorted
 
-# # wirte appList to sqlite
+# write appArray to csv
+with open('timedate.csv', 'wb') as f:
+	w = csv.DictWriter(f, appArray.keys())
+	w.writeheader()
+	w.writerow(appArray)
+
+# # wirte appArray to sqlite
 # conn = sqlite3.connect('appList.db')
 # c = conn.cursor()
 # # c.execute("""CREATE TABLE appList (id integer primary key,name varchar(20) UNIQUE,time integer)""")
